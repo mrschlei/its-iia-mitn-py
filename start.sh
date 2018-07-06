7c24e19a24675eb156751cdbc74ba9a15f32f763
Ben Fairfield <benfa@umich.edu>
2018-07-06 10:28:36 -0400

    Dockerfile will  run start.sh. start.sh will not run local.start.sh

--- start.sh
+++ start.sh
@@ -7,18 +7,17 @@ ln -sf /dev/stderr /var/log/apache2/error_log
 ln -sf /secrets/apache2/apache2.conf /etc/apache2/apache2.conf
 ln -sf /secrets/apache2/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf
 ln -sf /secrets/apache2/cosign.conf /etc/apache2/mods-available/cosign.conf
-#ln -sf /secrets/apache2/apache2-foreground /usr/local/bin/apache2-foreground
-#ln -sf /secrets/apache2/ports.conf /etc/apache2/ports.conf
+ln -sf /secrets/apache2/ports.conf /etc/apache2/ports.conf
 
 # SSL secrets
 ln -sf /secrets/ssl/USERTrustRSACertificationAuthority.pem /etc/ssl/certs/USERTrustRSACertificationAuthority.pem
 ln -sf /secrets/ssl/AddTrustExternalCARoot.pem /etc/ssl/certs/AddTrustExternalCARoot.pem
 ln -sf /secrets/ssl/sha384-Intermediate-cert.pem /etc/ssl/certs/sha384-Intermediate-cert.pem
 
-if [ -f /secrets/app/local.start.sh ]
-then
-  /bin/sh /secrets/app/local.start.sh
-fi
+#if [ -f /secrets/app/local.start.sh ]
+#then
+#  /bin/sh /secrets/app/local.start.sh
+#fi
 
 ## Rehash command needs to be run before starting apache.
 c_rehash /etc/ssl/certs >/dev/null
